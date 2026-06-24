1c70213d88bef8f642fc8bff0f6ceca51a79a560
Brian Simiyu <briansimiyuj@gmail.com>
2026-06-24 12:52:45 +0300

    Piping streams

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 import fs from "fs"
 
 const readStream = fs.createReadStream("./files/lorem.txt", { encoding: "utf-8" }),
-      writeStream = fs.createWriteStream("./files/new-lorem.txt")
+      writeStream = fs.createWriteStream("./files/new-lorem-2.txt")
 
 readStream.on("data", chunk =>{
 
     writeStream.write(chunk)
     
 })
+
+readStream.pipe(writeStream)
