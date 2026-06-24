e5d89a54a9d971434b842aba2baa06dd10bf723e
Brian Simiyu <briansimiyuj@gmail.com>
2026-06-24 12:50:16 +0300

    Streams

--- script.js
+++ script.js
@@ -1,29 +1,10 @@
-import fsPromises from "fs/promises"
+import fs from "fs"
 
-const fileOps = async() =>{
+const readStream = fs.createReadStream("./files/lorem.txt", { encoding: "utf-8" }),
+      writeStream = fs.createWriteStream("./files/new-lorem.txt")
 
-    try{
+readStream.on("data", chunk =>{
+
+    writeStream.write(chunk)
     
-        const data = await fsPromises.readFile("./files/starter.txt", "utf8")
-
-        await fsPromises.unlink("./files/starter.txt")
-
-        await fsPromises.writeFile("./files/promiseWrite.txt", "Jesus is the King of kings")
-
-        await fsPromises.appendFile("./files/promiseWrite.txt", "\nJesus is the Lord of lords")
-
-        await fsPromises.rename("./files/promiseWrite.txt", "./files/promiseRename.txt")
-
-        const newData = await fsPromises.readFile("./files/promiseRename.txt", "utf8")
-
-        console.log(newData)
-    
-    }catch(e){
-    
-        console.log(e)
-    
-    }
-
-}
-
-fileOps()
+})
