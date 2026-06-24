4f6c6293f6869c3f6ce55ddc4bb52b52bf57cf0b
Brian Simiyu <briansimiyuj@gmail.com>
2026-06-24 12:06:40 +0300

    Updating a file

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ fs.writeFile("./files/reply.txt", "This is a new file", err =>{
     console.log("File has been written")
 
 })
+
+fs.appendFile("./files/reply.txt", "This is a new line", err =>{
+
+    if(err) throw err
+
+    console.log("File has been appended")
+    
+})
