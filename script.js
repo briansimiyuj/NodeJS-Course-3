e1419c5616e8c1252a39511e6b8a03fec016f2a9
Brian Simiyu <briansimiyuj@gmail.com>
2026-06-25 13:01:27 +0300

    Creating a server

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 import logEvents from "./logEvents.js"
 import EventEmitter from "events"
+import http from "http"
+import path from "path"
 
-const emitter = new EventEmitter()
+const emitter = new EventEmitter(),
+      PORT = process.env.PORT || 3000
 
-emitter.on("log", message => logEvents(message))
+const server = http.createServer((req, res) =>{
 
-setTimeout(() =>{
+    res.writeHead(200, {"Content-Type": "text/html"})
 
-    emitter.emit("log", "log event emitted")
-    
-}, 2000)
+    res.end(`<h1>Hello World</h1>`)
+
+    console.log(`Request method: ${req.method}, Request URL: ${req.url}`)
+
+})
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
