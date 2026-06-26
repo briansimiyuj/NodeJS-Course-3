503084e0bd852699c1f18671888a71b010047fad
Brian Simiyu <briansimiyuj@gmail.com>
2026-06-26 11:52:23 +0300

    Introduction to express

--- script.js
+++ script.js
@@ -3,135 +3,17 @@ import EventEmitter from "events"
 import http from "http"
 import path from "path"
 import fs from "fs"
+import express from "express"
 
 const emitter = new EventEmitter(),
       PORT = process.env.PORT || 3000
 
-emitter.on("log", (message, fileName) => logEvents(message, fileName))
+const app = express()
 
-const serveFile = async(filePath, contentType, response) =>{
+app.get('/', (req, res) =>{
 
-    try{
-
-        const rawData = await fs.promises.readFil(
-            filePath, 
-            !contentType.includes("image") ? "utf-8" : undefined
-        )
-
-        response.writeHead(
-            filePath.includes("404.html") ? 404 : 200,
-            { "Content-Type": contentType }
-        )
-
-        response.end(rawData)
-
-    }catch(err){
-
-        console.log(err)
-
-        response.statusCode = 500 
-
-        emitter.emit("log", `${err.name}\t${err.message}`, "errLog")
-
-        response.end()
-
-    }
-
-}
-
-const server = http.createServer((req, res) =>{
-
-    emitter.emit("log", `${req.url}\t${req.method}`,  "reqLog")
-
-    const extension = path.extname(req.url)
-
-    let contentType
-
-    switch(extension){
-        
-        case ".css":
-
-            contentType = "text/css"
-
-        break
-
-        case ".js":
-
-            contentType = "text/javascript" 
-
-        break   
-
-        case ".json":
-
-            contentType = "application/json"
-
-        break
-
-        case ".jpg":
- 
-            contentType = "image/jpeg"
-
-        break
-
-        case ".png":
-
-            contentType = "image/png"
-
-        break
-
-        case ".txt":
-
-            contentType = "text/plain"
-
-        break
-
-        default:
-
-            contentType = "text/html"
-
-    }
-
-    let filePath = 
-        contentType === "text/html" && req.url === "/" ? "views/index.html" :
-        contentType === "text/html" && req.url.slice(-1) === "/" ? `views${req.url.slice(0, -1)}.html` :
-        contentType === "text/html" ? `views${req.url}` : contentType === "text/html" ? `views${req.url}.html` : `.${req.url}`
-
-    if(!extension && req.url.slice(-1) !== "/") filePath += ".html"
-
-    const fileExists = fs.existsSync(filePath)
-
-    if(fileExists){
-
-        serveFile(filePath, contentType, res)
-
-    }else{
-
-        switch(path.parse(filePath).base){
-
-            case "old-page.html":
-
-                res.writeHead(301, { "Location": "/new-page.html" })
-
-                res.end()
-
-            break
-
-            case "www-page.html":
-
-                res.writeHead(301, { "Location": "/" })
-
-            break
-
-            default:
-
-                serveFile("views/404.html", "text/html", res)
-
-        }
-
-    }
-
-    console.log(req.url, req.method, res.statusCode)
-
+    res.send('Hello World')
+    
 })
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
