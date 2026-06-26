df94e5c15ba6202422b2a35c81056bbe1682ef73
Brian Simiyu <briansimiyuj@gmail.com>
2026-06-26 13:19:16 +0300

    Serving HTML Files

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 import logEvents from "./logEvents.js"
 import EventEmitter from "events"
 import http from "http"
-import path from "path"
+import path, { dirname, join } from "path"
 import fs from "fs"
 import express from "express"
+import { fileURLToPath } from "url"
 
 const emitter = new EventEmitter(),
-      PORT = process.env.PORT || 3000
+      PORT = process.env.PORT || 3000,
+      __fileName = fileURLToPath(import.meta.url),
+      __dirName = dirname(__fileName)
 
 const app = express()
 
-app.get('/', (req, res) =>{
+app.get("/", (req, res) =>{
 
-    res.send('Hello World')
+    res.sendFile(join(__dirName, "views/index.html"))
     
+})
+
+app.get(["/new-page", "/new-page.html"], (req, res) =>{
+
+    res.sendFile(join(__dirName, "views/new-page.html"))
+
+})
+
+app.get(["/old-page", "/old-page.html"], (req, res) =>{
+
+    res.redirect(301, '/new-page')
+
+})
+
+app.use((req, res) =>{
+
+    res.status(404).sendFile(join(__dirName, "views/404.html"))
+
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
