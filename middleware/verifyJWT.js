bcdd8a99d33804d154a44a74f5ab49735dc7f448
Brian Simiyu <briansimiyuj@gmail.com>
2026-07-10 12:15:24 +0300

    Add verifyRoles middleware to check user roles for resource access

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -7,9 +7,7 @@ const verifyJWT = (req, res, next) =>{
 
     const authHeader = req.headers.authorization || req.headers.Authorization
 
-    if(!authHeader) return res.sendStatus(401)
-
-    console.log(authHeader)
+    if(!authHeader?.startsWith("Bearer ")) return res.sendStatus(401)
 
     const token = authHeader.split(" ")[1]
 
@@ -17,7 +15,9 @@ const verifyJWT = (req, res, next) =>{
 
         if(err) return res.sendStatus(403)
 
-        req.user = decoded.username
+        req.user = decoded.UserInfo.username
+
+        req.roles = decoded.UserInfo.roles
 
         next()
 
