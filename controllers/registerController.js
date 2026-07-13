dc9e414f469fd730f2283e0ef3667eb7b0ec5968
Brian Simiyu <briansimiyuj@gmail.com>
2026-07-13 15:20:00 +0300

    Refactor user registration to use MongoDB for user storage

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,21 +1,5 @@
-import express from "express"
-import fsPromises from "fs/promises"
-import path, { dirname, join } from "path"
 import bcrypt from "bcrypt"
-import users from "../public/model/employees.json" with { type: "json" }
-import { fileURLToPath } from "url"
-
-const __fileName = fileURLToPath(import.meta.url),
-      __dirName = path.dirname(__fileName),
-      usersFilePath = join(__dirName, "../public/model/employees.json")
-
-const userDB ={
-
-    users: JSON.parse(await fsPromises.readFile(usersFilePath, "utf-8")),
-    setUsers: function(data){ this.users = data }
-
-}
-
+import User from "../public/model/User.js"
 
 const handleNewUser = async(req, res) =>{
 
@@ -23,7 +7,7 @@ const handleNewUser = async(req, res) =>{
 
     if(!username || !password) return res.status(400).json({ "message": "Username and password are required." })
 
-    const duplicate = userDB.users.find(person => person.username === username)
+    const duplicate = await User.findOne({ "username": username }).exec()
 
     if(duplicate) return res.sendStatus(409) 
 
@@ -31,22 +15,12 @@ const handleNewUser = async(req, res) =>{
     
         const hashedPassword = await bcrypt.hash(password, 10)  
         
-        const newUser ={ 
+        const newUser = await User.create({ 
             "username": username, 
-            "password": hashedPassword,
-            "roles": { "User": 2001 } 
-        }
+            "password": hashedPassword
+        })
 
-        userDB.setUsers([...userDB.users, newUser])
-
-        await fsPromises.writeFile(
-
-            usersFilePath,
-            JSON.stringify(userDB.users)
-
-        )
-
-        console.log(userDB.users)
+        console.log(newUser)
 
         res.status(201).json({ "success": `New user ${username} created!` })
 
